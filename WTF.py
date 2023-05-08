0c4afd9c2468f5900a6be9f6ca37cfed108da1c8
gabradle7 <garrett.bradley77@gmail.com>
2023-05-08 16:04:30 -0400

    wtf

--- WTF.py
+++ WTF.py
@@ -1,4 +1,9 @@
-count=0
-while count<1000000:
-    print (count)
-    count+=1
+#Assign students to either group 1 or 2.  Alternate in the placement of the students.  There are 457 students.  Hint: Evens and odds
+def placement(student):
+    count=0
+    
+    if student%2==0:
+        return student
+    else:
+        return "Odd"
+print(placement(457))
