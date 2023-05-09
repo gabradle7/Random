c87fc13c9200cfa5e2d77410377b866073d523c5
gabradle7 <garrett.bradley77@gmail.com>
2023-05-09 06:34:45 -0400

    l

--- WTF.py
+++ WTF.py
@@ -1,9 +1,11 @@
 #Assign students to either group 1 or 2.  Alternate in the placement of the students.  There are 457 students.  Hint: Evens and odds
 def placement(student):
-    count=0
-    
-    if student%2==0:
-        return student
-    else:
-        return "Odd"
+    x=range(1,int(student))
+    for i in x:
+        if i%2==0:
+            return 1
+        else:
+            return 2 
 print(placement(457))
+#for i in range(457):
+  #  print(i)
